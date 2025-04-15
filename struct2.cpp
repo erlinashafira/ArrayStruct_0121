4bcd22ae42c821bc2a1fcf5115ecee6dcced5930
errerew <erlina31acc@gmail.com>
2025-04-15 19:11:46 +0700

    bagian akhir

--- struct2.cpp
+++ struct2.cpp
@@ -11,3 +11,31 @@ string nim;
 string nama;
 detailAlamat alamat;
 };
+
+int main(){
+      //membuat object struct mahasiswa
+      mahasiswa mhs;
+
+      //mengaksesstruct mahaasiswa
+      mhs.nim = "20201400";
+      mhs.nama = "Abra";
+      mhs.alamat = "Kasihan";
+
+      cout << "Masukkan NIM = ";
+      cin >> mhs.nim;
+      cout << "Masukkan Nama = ";
+      cin >> mhs.nama;
+      cout << "Masukkan ALAMAT DESA = ";
+      cin >> mhs.alamat.desa;
+      cout << "Masukkan ALAMAT KOTA = ";
+      cin >> mhs.alamat.kota;
+
+      cout << endl;
+      cout << "Data Mahasiswa " <, endl;
+
+      cout << "NIM = " << mhs.nim << endl;
+      cout << "ANAMA = " << mhs.nama << endl;
+      cout << "ALAMAT DESA = " <<mhs.alamat.desa << endl;
+      cout << "ALAMAT KOTA = " <<mhs.alamat.kota << endl;
+
+}
