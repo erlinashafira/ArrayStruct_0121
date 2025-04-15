5db368d8418900db7f63039d5e3414b2ff3f0d2b
errerew <erlina31acc@gmail.com>
2025-04-15 19:18:46 +0700

    penambahan getline

---
+++ structGetLine.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    sring kota;
+};
+
+struct mahasiswa{
+string nim;
+string nama;
+detailAlamat alamat;
+};
+
+int main(){
+      //membuat object struct mahasiswa
+      mahasiswa mhs;
+
+      cout << "Masukkan NIM = ";
+      cin >> mhs.nim;
+      cin.ignore();
+      cout << "Masukkan NAMA = ";
+      getline (cin , mhs.nama);
+      cout << "Masukkan ALAMAT DESA = ";
+      cin >> mhs.alamat.desa;
+      cout << "Masukkan ALAMAT KOTA = ";
+      cin >> mhs.alamat.kota;
+
+      cout << endl;
+      cout << "Data Mahasiswa " <, endl;
+
+      cout << "NIM = " << mhs.nim << endl;
+      cout << "ANAMA = " << mhs.nama << endl;
+      cout << "ALAMAT DESA = " <<mhs.alamat.desa << endl;
+      cout << "ALAMAT KOTA = " <<mhs.alamat.kota << endl;
+
+}
